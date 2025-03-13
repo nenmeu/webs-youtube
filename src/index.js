a2bd5dea4fb0d297fa80c4c179d0a222ee4e9342
nenmeu <hanuel0525@gmail.com>
2025-03-13 09:16:17 +0900

    헤더영역완성

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './assets/scss/style.scss'
 
-import './assets/scss/style.scss'
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
