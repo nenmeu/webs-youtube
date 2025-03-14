e60afb2961a9d7c974e0b50c410d9263ccc65a29
nenmeu <hanuel0525@gmail.com>
2025-03-14 17:48:11 +0900

    2025-03-14

---
+++ src/components/video/VideoSlider.jsx
@@ -0,0 +1,63 @@
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import { Navigation } from 'swiper/modules';
+
+
+const VideoSlider = ({id, title, videos}) => {
+    const [loading, setLoading]=useState(true)
+    useEffect(()=>{
+        setTimeout(()=>{
+            setLoading(false)
+        },500)
+    },[])
+    const videoClass = loading ? 'isLoading' : 'isLoaded'
+    return (
+        <section id={id} className={videoClass}>
+            <h2>{title}</h2>
+            <div className='video_slider'>
+            <Swiper 
+            slidesPerView={1}
+            spaceBetween={20}
+            navigation={true} 
+            modules={[Navigation]} 
+            className={`mySwiper-${id}`}
+            breakpoints={{
+                1600:{
+                    slidesPerView:5,
+                    spaceBetween:20
+                },
+                1024:{
+                    slidesPerView:4,
+                    spaceBetween:20
+                },
+                768:{
+                    slidesPerView:3,
+                    spaceBetween:20
+                },
+                640:{
+                    slidesPerView:2,
+                    spaceBetween:20
+                }
+            }}
+            >
+                {videos.map((video, key)=>(
+                    <SwiperSlide key={key}>
+                        <div className='video'>
+                        <div className='video_thumb play_icon'>
+                            <Link to={`/video/${video.videoId}`}>
+                            <img src={video.img} alt={video.title}/>
+                            </Link>
+                        </div>
+                        </div>
+                    </SwiperSlide>
+                ))}
+            </Swiper>
+            </div>
+        </section>  
+    )
+}
+
+export default VideoSlider
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,20 +2,24 @@ import React from 'react'
 import Main from '../components/section/Main'
 import Today from '../components/content/Today'
 import Music from '../components/content/Music'
-import Movie from '../components/content/Movie'
-import Book from '../components/content/Book'
-import Anime from '../components/content/Anime'
-import Portfolio from '../components/content/Portfolio'
-import Youtube from '../components/content/Youtube'
+import VideoSlider from '../components/video/VideoSlider'
+import { movieText } from '../data/movie'
+import { BookText } from '../data/book'
+import { AnimeText } from '../data/anime'
 
 const Home = () => {
   return (
     <Main title="유튜브 채널" description="유튜브 채널 페이지입니다.">
       <Today/>
       <Music/>
-      <Movie/>
+      <VideoSlider videos={movieText} title='(ﾉ*ФωФ)ﾉ추천 영화를 소개합니다.' id='movie'/>
+      <VideoSlider videos={BookText} title='（づ￣3￣）づ추천 도서를 소개합니다.' id='book'/>
+      <VideoSlider videos={AnimeText} title='o(￣▽￣)ｄ 추천 애니를 소개합니다.' id='anime'/>
+      {/* <VideoSlider id='book'/>
+      <VideoSlider id='anime'/> */}
+      {/* <Movie/>
       <Book/>
-      <Anime/>
+      <Anime/> */}
       {/* <Portfolio/>
       <Youtube/> */}
     </Main>
