3191c512e071a5b102a20d529a200dcf383f88c4
nenmeu <hanuel0525@gmail.com>
2025-03-14 16:29:23 +0900

    메인 섹션 작업

--- src/components/content/Anime.jsx
+++ src/components/content/Anime.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import { AnimeText } from '../../data/anime'
+import { Link } from 'react-router-dom'
 
 const Anime = () => {
   return (
-    <div>Anime</div>
+        <section id='movie'>
+          <h2>o(￣▽￣)ｄ 추천 애니를 소개합니다.</h2>
+          <div className='video_inner'>
+            {AnimeText.map((video, key)=>(
+              <div className='video' key={key}>
+                <div className='video_thumb play_icon'>
+                  <Link to={'/video/$video.videoId'}>
+                    <img src={video.img} alt={video.title}/>
+                  </Link>
+                </div>
+              </div>
+            ))}
+          </div>
+        </section>  
   )
 }
 
--- src/components/content/Book.jsx
+++ src/components/content/Book.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import { BookText } from '../../data/book'
+import { Link } from 'react-router-dom'
 
 const Book = () => {
   return (
-    <div>Book</div>
-  )
+    <section id='movie'>
+      <h2>（づ￣3￣）づ추천 도서를 소개합니다.</h2>
+      <div className='video_inner'>
+        {BookText.map((video, key)=>(
+          <div className='video' key={key}>
+            <div className='video_thumb play_icon'>
+              <Link to={'/video/$video.videoId'}>
+                <img src={video.img} alt={video.title}/>
+              </Link>
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>  
+    )
 }
 
 export default Book
