351238788fd4eaf5eb3bccb661aae6a330f9cc60
nenmeu <hanuel0525@gmail.com>
2025-03-14 10:44:28 +0900

    메인 컨텐츠 반응

--- src/pages/Today.jsx
+++ src/pages/Today.jsx
@@ -1,11 +1,45 @@
 import React from 'react'
 import Main from '../components/section/Main'
+import { todayText, TodayText } from '../data/Today'
+import { Link } from 'react-router-dom'
 
 const Today = () => {
   return (
-    <Main title="추천 영상" description="영상 추천 페이지입니다.">
-    Today
-  </Main>
+    <Main 
+    title="추천 영상" 
+    description="영상 추천 페이지입니다.">
+      <section id='todayPage'>
+        <h2>(☞ﾟヮﾟ)☞오늘의 추천 영상입니다.</h2>
+        {todayText.map((today,key)=>{
+          return(
+            <div className='today_inner' key={key}>
+              <div className='today_thumb play_icon'>
+                <Link to={today.page}>
+                  <img src={today.img} alt={today.title}/>
+                </Link>
+              </div>
+              <div className='today_text'>
+                <span className='today'>today</span>
+                <h3 className='title'>
+                  <Link to={today.page}>
+                    {today.title}
+                  </Link>
+                </h3>
+                <p className='desc'>{today.desc}</p>
+                <div className='info'>
+                  <span className='author'>
+                    <Link to={`/channel/${today.channelId}`}>
+                      {today.author}
+                    </Link>
+                  </span>
+                  <span className='date'>{today.date}</span>
+                </div>
+              </div>
+            </div>
+          )
+        })}
+      </section>
+    </Main>
   )
 }
 
